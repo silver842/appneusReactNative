a3003189cb1d1ea5a3786982c5ef90fb89618118
Bruno Silveira <silveira.bruno.br@gmail.com>
2019-09-30 20:54:19 -0300

    Setting up Welcome Page with React Navigator

--- App.js
+++ App.js
@@ -2,14 +2,41 @@ import React, { Component } from 'react';
 import { 
     StyleSheet,
     Text,
-    View 
+    View,
 } from 'react-native';
+import { Navigator } from 'react-native-deprecated-custom-components';
 
-export default function App() {
-  return (
-    <View>
-      <Text>Bem Vindo ao APPNEUS!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render(){
+     return (
+        <Navigator initialRoute = {{id: 'WelcomePage'}} 
+        renderScene = {this.navigatorRenderScene}/>
+    );
+  }
+
+  navigatorRenderScene(route, navigator){
+    switch (route.id) {
+      case 'WelcomePage':
+        return ( <WelcomePage navigator = {navigator} />)
+    }
+  }
+  
 }
 
+class WelcomePage extends Component{
+  render(){
+    return(
+      <View>
+        <Text>Bem Vindo!</Text>
+      </View>
+    );
+  }
+}
+
+
+const Styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    marginLeft: 20,
+  },
+})
